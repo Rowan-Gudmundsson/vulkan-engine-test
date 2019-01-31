66d1de654b952b6157f215745d937e578b6a41e6
Rowan Gudmundsson <rgudmundsson@nevada.unr.edu>
2019-01-31 13:27:32 -0800

    Adding graphics pipeline for triangle example

---
+++ include/graphics_headers.h
@@ -0,0 +1,17 @@
+#include "vulkan.hpp"
+
+#include <GLFW/glfw3.h>
+
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <optional>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#define DEV_MODE
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "triangle.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+
+#include <iostream>
+
+int main() {
+  Triangle triangle;
+
+  try {
+    triangle.Run();
+  } catch (const std::exception& e) {
+    std::cout << "Error occurred: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
